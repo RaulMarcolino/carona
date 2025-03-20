5b23524557edd85232d846b7c003a78ddcda9cd8
Raul <luiz.junior53@aluno.ce.gov.br>
2025-03-20 08:50:38 -0300

    Commit Inicial

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Vai Trabaiá!</Text>
       <StatusBar style="auto" />
     </View>
   );
