0798333dd7b38a8bdcf0278e46b48f8b9dd73628
Raul Marcolino <luiz.junior53@aluno.ce.gov.br>
2025-05-08 16:32:08 -0300

    style update

--- app/(tabs)/travels.tsx
+++ app/(tabs)/travels.tsx
@@ -1,19 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Travels = () => {
     return (
         <View>
-          <View>
-            <View>
 
-            </View>
+          <View style = {styles.travelscard}>
+
             <View>
-              
+              <Text>De: Cedro</Text>
+              <Text>Para: Fortaleza</Text>
+              <Text>Data: 20/09/2025</Text>
+              <Text>Horário: 08:00h</Text>
             </View>
+
+            <View>
+              <Text>Vagas</Text>
+              <Text>3/4</Text>
+            </View>
+
           </View>
+
         </View>
     )
 }
+const styles = StyleSheet.create({
+  travelscard: {
+    backgroundColor: '#9e9e9e',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 20,
+    marginHorizontal: 20
+  }
+})
 
 export default Travels
