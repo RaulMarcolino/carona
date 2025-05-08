c9f3fc3ec3c279fdee6ff7be3fca5a7f2e1b517c
Raul Marcolino <luiz.junior53@aluno.ce.gov.br>
2025-05-08 16:20:26 -0300

    header title and views

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,44 @@
+import { AntDesign, Feather } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 
 export default function Layout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{ tabBarStyle: { backgroundColor: '#000' }, headerStyle: { backgroundColor: '#000' } }}>
             <Tabs.Screen
                 name="home"
-               
-            />
-            <Tabs.Screen
-                name="settings"
-                
+                options={{
+                  title: '',
+                  headerTitle: 'Home',
+                    headerTitleStyle: {
+                        color: '#fff'
+                    },
+                  tabBarIcon: ({ color }) => <Feather size={28} name="home" color={color} />,
+                }}
             />
 
             <Tabs.Screen
                 name="travels"
-                
+                options={{
+                  title: '',
+                  headerTitle: 'Minhas Caronas',
+                    headerTitleStyle: {
+                        color: '#fff'
+                    },
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="map-o" color={color} />,
+                }}
+            />
+
+            <Tabs.Screen
+                name="settings"
+                options={{
+                  title: '',
+                  headerTitle: 'Meu Perfil',
+                    headerTitleStyle: {
+                        color: '#fff'
+                    },
+                  tabBarIcon: ({ color }) => <AntDesign size={28} name="setting" color={color} />,
+                }}
             />
         </Tabs>
     );
